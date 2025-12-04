e4dc878682a6650ea170cf9dceb5078d647f2f0f
Alexander Urrutia <matias04041994@gmail.com>
2025-12-04 14:42:45 -0300

    fix: update Vercel adapter import to use standard version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   integrations: [tailwind()],
