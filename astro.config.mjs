91f987de123af6d92efb427864cf6be84bcf92d6
Alexander Urrutia <matias04041994@gmail.com>
2025-12-04 14:40:19 -0300

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/edge";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
